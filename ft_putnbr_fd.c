748b0894f30ea1fb8956691a1729cc18c13c310d
Adrien Pachkoff <apachkof@student.42.fr>
2015-05-20 22:07:08 +0200

    new ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: apachkof <apachkof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/22 18:28:54 by apachkof          #+#    #+#             */
-/*   Updated: 2015/05/19 21:14:55 by apachkof         ###   ########.fr       */
+/*   Updated: 2015/05/20 22:03:44 by apachkof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,25 @@
 
 void	ft_putnbr_fd(int num, int fd)
 {
-	char	c;
-	int		weight;
+	char			c;
+	unsigned int	weight;
 
-	if (num == 0)
+	if (num == -2147483648)
 	{
-		write(fd, "0", 1);
+		write(fd, "-2147483648", 11);
 		return ;
 	}
 	if (num < 0)
-		write(fd, "-", 1);
-	else
-		num = -num;
-	weight = 1;
-	while ((num / (10 * weight)) != 0)
 	{
-		weight *= 10;
+		write(fd, "-", 1);
+		num = -num;
 	}
+	weight = 1;
+	while ((unsigned int)num / 10 / weight != 0)
+		weight *= 10;
 	while (weight != 0)
 	{
-		c = -(num / weight) % 10 + '0';
+		c = (char)(((unsigned int)num / weight) % 10) + '0';
 		write(fd, &c, 1);
 		weight /= 10;
 	}
